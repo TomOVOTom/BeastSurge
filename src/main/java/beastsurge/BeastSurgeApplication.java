6d8585544cf6cc70c66589cb8a65902d1728886a
Administrator <3357862769@qq.com>
2025-01-30 04:28:17 +0800

    Initial commit

---
+++ src/main/java/beastsurge/BeastSurgeApplication.java
@@ -0,0 +1,13 @@
+package beastsurge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeastSurgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeastSurgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/beastsurge/BeastSurgeApplicationTests.java
@@ -0,0 +1,13 @@
+package beastsurge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeastSurgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
